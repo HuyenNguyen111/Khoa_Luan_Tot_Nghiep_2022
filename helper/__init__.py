71169bb7fd355cf99ca4f777e8e6bd4d368ee665
DVNghiem <vannghiem848@gmail.com>
2022-11-03 16:25:50 +0700

    update model

--- helper/__init__.py
+++ helper/__init__.py
@@ -2,7 +2,9 @@ from .detect import detect
 from .utils import allocate_buffers
 import tensorrt as trt
 
-TRT_LOGGER = trt.Logger()
+# TRT_LOGGER = trt.Logger()
+TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+trt.init_libnvinfer_plugins(TRT_LOGGER, namespace="")
 with open("./model_fp16.engine", "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
     engine = runtime.deserialize_cuda_engine(f.read())
 
