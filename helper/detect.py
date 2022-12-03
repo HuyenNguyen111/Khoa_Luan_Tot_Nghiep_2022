5588256d839af0ed94d8874e456fe46b352e2af9
DVNghiem <vannghiem848@gmail.com>
2022-12-03 17:28:02 +0700

    update fps

--- helper/detect.py
+++ helper/detect.py
@@ -3,11 +3,11 @@ import cv2
 from .utils import post_processing, plot_boxes_cv2
 import threading
 import os
+import time
 
 IN_IMAGE_H = 416
 IN_IMAGE_W = 416
-CLASS_NAME = "platic_box"
-
+CLASS_NAME = ["Plastic_bottle", "Plactic_bottle_detergent", "Plastic_box", "Plastic_bottle_yogurt", "Can", "Foam_box", "Face_mask"]
 
 labels = [0, 1, 2, 3, 4]
 
@@ -20,19 +20,22 @@ class CameraThread(threading.Thread):
 
     def run(self) -> None:
         vid = cv2.VideoCapture(0)
+        prev_frame_time = 0
+        new_frame_time = 0
         while vid.isOpened():
             quantity = 0
             _, frame = vid.read()
+            resized = cv2.resize(frame, (IN_IMAGE_W, IN_IMAGE_H),
+                            interpolation=cv2.INTER_LINEAR)
+            img_in = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+            img_in = np.transpose(img_in, (2, 0, 1)).astype(np.float32)
+            img_in = np.expand_dims(img_in, axis=0)
+            img_in /= 255.0
+            outputs = self.session.run(None, {"input": img_in})
+            boxes = post_processing(img_in, 0.4, 0.6, outputs)
+            img = plot_boxes_cv2(frame, boxes[0], class_names=CLASS_NAME)
             if os.path.exists('count/request.txt'):
                 os.remove('count/request.txt')
-                resized = cv2.resize(frame, (IN_IMAGE_W, IN_IMAGE_H),
-                             interpolation=cv2.INTER_LINEAR)
-                img_in = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
-                img_in = np.transpose(img_in, (2, 0, 1)).astype(np.float32)
-                img_in = np.expand_dims(img_in, axis=0)
-                img_in /= 255.0
-                outputs = self.session.run(None, {"input": img_in})
-                boxes = post_processing(img_in, 0.4, 0.6, outputs)
                 for i in range(len(boxes[0])):
                     box = boxes[0][i]
                     if len(box) >= 7:
@@ -40,11 +43,18 @@ class CameraThread(threading.Thread):
                         cls_id = box[6]
                         if cls_conf > 0.5 and cls_id in labels:
                             quantity += 1
-
                 with open('count/response.txt', 'w') as f:
                     f.write(f'{quantity}')
+            new_frame_time = time.time()
+            fps = 1/(new_frame_time-prev_frame_time)
+            prev_frame_time = new_frame_time
+            fps = int(fps)
+            fps = 'FPS: '+str(fps)
 
-            cv2.imshow('frame', frame)
+            img = cv2.putText(img, fps, (7, 35), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+
+
+            cv2.imshow('frame', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         vid.release()
--- main.py
+++ main.py
@@ -18,7 +18,6 @@ app = Flask(__name__)
 app.register_blueprint(bp)
 
 # List = ["Recycling", "Nonrenewable"]
-labels = ["Plastic_bottle", "Plactic_bottle_detergent", "Plastic_box", "Plastic_bottle_yogurt", "Can", "Foam_box", "Face_mask"]
 
 
 @app.route('/count')
